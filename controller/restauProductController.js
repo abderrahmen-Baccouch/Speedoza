0a451bb73d83619df042e17746764466686c7782
Yassinemessaoud9 <medyassinee.messaoud@esprit.tn>
2024-08-09 23:32:07 +0100

    fix id

--- controller/restauProductController.js
+++ controller/restauProductController.js
@@ -9,7 +9,7 @@ export async function createRestauProduct(req, res) {
 
   try {
     const user = await User.findById(userId);
-    const foodItem = await Foood.findById(foodId); // Renamed to avoid conflict
+    const foodItem = await Foood.findById({_id:foodId}); // Renamed to avoid conflict
     console.log(foodId);
     if (!foodItem) {
       return res.status(404).json({ message: " Food item not found" });
