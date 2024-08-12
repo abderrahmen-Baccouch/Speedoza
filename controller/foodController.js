57bab5ef1b6b588e283c9e381ab0f63cde667cbf
Yassinemessaoud9 <medyassinee.messaoud@esprit.tn>
2024-08-12 15:26:05 +0100

    fix cat

--- controller/foodController.js
+++ controller/foodController.js
@@ -2,28 +2,34 @@
 import Food from '../models/food.js';
 import Category from '../models/category.js';
 
-
-export async function createFood (req, res)  {
-    const { name, description,cateogryId } = req.body;
-    const photos = req.files.map(file => `${req.protocol}s://${req.get('host')}/public/images/${file.filename}`);
+export async function createFood(req, res) {
+    const { name, description, cateogryId } = req.body;
+    const photos = req.files.map(file => `${req.protocol}://${req.get('host')}/public/images/${file.filename}`);
 
     try {
-const category = await Category.findById({_id:cateogryId});
-if (!category) {
-    return res.status(404).json({ message: "Category not found" });
-}
+        const category = await Category.findById(cateogryId);
+        if (!category) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
         const newFood = new Food({
             name,
             description,
             photos,
             category: category._id
         });
+
         await newFood.save();
-        //update category foods array with new food id
+
+        // Update the category's foods array with the new food id
         category.foods.push(newFood._id);
+
+        // Save the updated category document
+        await category.save();
+
         res.status(201).json({ message: "Food item created successfully", food: newFood });
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).json({ message: "Server Error" });
     }
 };
