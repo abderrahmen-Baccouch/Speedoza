fdff322f624af85aaa0a39634e68a89ab1f48ff3
Yassinemessaoud9 <medyassinee.messaoud@esprit.tn>
2024-08-13 23:09:06 +0100

    fix config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,7 +1,7 @@
 import cloudinary from 'cloudinary';
 
 cloudinary.config({
-    cloud_name: 'speedoza',
+    cloud_name: 'dm7zwfhxr',
     api_key: '642885729211789',
     api_secret: 'a2ct8PyVx0y5bzkdOnL_HIFoBNk'
 });
