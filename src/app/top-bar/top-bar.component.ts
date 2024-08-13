d00302f06fdde55878fe7ec4307f15660cb5bf3d
abderrahmen <baccouchabderrahmen@gmail.com>
2024-08-13 11:25:33 +0100

    responsive first commit

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router'; 
+import { AuthService } from '../service/services.service';
 @Component({
   selector: 'app-top-bar',
   standalone: true,
@@ -9,5 +11,11 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './top-bar.component.css'
 })
 export class TopBarComponent {
+  constructor(private router: Router,  private authService: AuthService , ) { }
+  logout() {
+    console.log('Logout method called in AuthService');
+    this.authService.logout(); 
+    this.router.navigate(['/login']);
+  }
 
 }
