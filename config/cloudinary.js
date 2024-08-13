50d121acb2d2d82ec5109165cd1b1561ae3d1d96
Yassinemessaoud9 <medyassinee.messaoud@esprit.tn>
2024-08-13 22:58:27 +0100

    fix

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinary from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'speedoza',
+    api_key: '642885729211789',
+    api_secret: 'a2ct8PyVx0y5bzkdOnL_HIFoBNk'
+});
+
+export default cloudinary;
