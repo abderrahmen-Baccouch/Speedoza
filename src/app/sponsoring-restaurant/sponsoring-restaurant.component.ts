883d4f32734915203b87a01ed71ccd9ce5cae8ea
abderrahmen17 <baccouchabderrahmen@gmail.com>
2024-08-26 13:56:45 +0100

    restaurant-profile

--- src/app/sponsoring-restaurant/sponsoring-restaurant.component.ts
+++ src/app/sponsoring-restaurant/sponsoring-restaurant.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-sponsoring-restaurant',
-  standalone: true,
   templateUrl: './sponsoring-restaurant.component.html',
   styleUrls: ['./sponsoring-restaurant.component.css']
 })
-export class SponsoringRestaurantComponent {}
+export class SponsoringRestaurantComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const items = document.querySelectorAll('.section-item');
+
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        items.forEach(el => el.classList.remove('selected'));
+        item.classList.add('selected');
+      });
+    });
+  }
+
+
+
+}
